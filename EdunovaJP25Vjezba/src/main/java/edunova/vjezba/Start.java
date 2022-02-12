e3682f912daeb8ceebb510a1fa5171ea9b1126d8
SGroznica <93283616+SGroznica@users.noreply.github.com>
2022-02-12 22:11:51 +0100

    Dodavanje classa

---
+++ EdunovaJP25Vjezba/src/main/java/edunova/vjezba/Start.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.vjezba;
+
+import edunova.util.HibernateUtil;
+
+/**
+ *
+ * @author stjep
+ */
+public class Start {
+    
+    public static void main(String[] args) {
+        HibernateUtil.geSession();
+    }
+    
+}
