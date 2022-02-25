9cca3f36b55199e33c37aad69591297e2c192a93
SGroznica <93283616+SGroznica@users.noreply.github.com>
2022-02-25 19:18:18 +0100

    Uređivanje klase

--- ZavrsniRad/src/main/java/zavrsnirad/util/HibernateUtil.java
+++ ZavrsniRad/src/main/java/zavrsnirad/util/HibernateUtil.java
@@ -24,7 +24,7 @@ public class HibernateUtil {
     
     public static Session getSession(){
         if(session==null){
-            //upogoni hibernate
+            
             try {
                 registry = new StandardServiceRegistryBuilder().configure().build();
                 MetadataSources sources = new MetadataSources(registry);
